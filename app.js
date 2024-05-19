be13c4496fdfa85c2a405dc614d776b2648eeea5
pradeepkumar128 <iampradeep1282@gmail.com>
2024-05-19 15:35:15 +0530

    done

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-let clockArea = document.querySelector('#digittime');
+let clockArea = document.querySelector("#digittime");
 
+function showTimes() {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  var session = "AM";
 
-function showTimes(){
-let date = new Date();
-let h = date.getHours() 
-let m = date.getMinutes();
-let s = date.getSeconds();
-var session = "AM";
+  if (h == 0) {
+    h = 12;
+  }
 
-if(h == 0){
-     h = 12;
- }
- 
- if(h > 12){
-     h = h - 12;
-     session = "PM";
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  let result = h + " : " + m + " : " + s + " " + session;
+  document.getElementById("digittime").innerText = result;
+  document.getElementById("digittime").textContent = result;
 }
 
-h = (h < 10) ? "0" + h : h;
-m = (m < 10) ? "0" + m : m;
-s = (s < 10) ? "0" + s : s;
-
-let result = h+' : '+ m +' : '+ s +' '+session;
-document.getElementById('digittime').innerText = result;
-document.getElementById("digittime").textContent = result;
-
-}
-
-
-setInterval(showTimes,1000)
+setInterval(showTimes, 1000);
